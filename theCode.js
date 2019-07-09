e9ac16c04d58313ce096f99e4b80837afa763b77
OscarHavril <OscarHavril@users.noreply.github.com>
2019-07-09 15:24:50 +0200

    Update theCode.js

--- theCode.js
+++ theCode.js
@@ -1,3 +1,4 @@
+var map = "eeeeeeeeaeaaaeeaaaeeeeaeaaaeeaeeeaeeaeeaaeeeeeeee"
 var x = 1;
 var y = 1;
 document.getElementById("load").innerHTML = "JavaScript working!";
@@ -5,20 +6,54 @@ document.getElementById("coordx").innerHTML = x;
 document.getElementById("coordy").innerHTML = y;
 
 function moveUp() {
-	document.getElementById("up").innerHTML ="OH NO A WALL"
-	document.getElementById("up").style.backgroundColor = "red";
-	y--;
-	document.getElementById("coordy").innerHTML = y;
-	}
+	if (map.substr(y-- * 6 + x,1) == "a") {
+		y--;
+		document.getElementById("down").style.backgroundColor = "green";
+		document.getElementById("left").style.backgroundColor = "green";
+		document.getElementById("right").style.backgroundColor = "green";
+		document.getElementById("up").style.backgroundColor = "green";
+		document.getElementById("coordy").innerHTML = y;
+	} else {
+		document.getElementById("up").style.backgroundColor = "red";
+		document.getElementById("up").innerHTML ="OH NO A WALL";
+	} 
+}
 function moveDown() {
-	y++;
-	document.getElementById("coordy").innerHTML = y;
-	}
+	if (map.substr(y++ * 6 + x,1) == "a") {
+		y++;
+		document.getElementById("down").style.backgroundColor = "green";
+		document.getElementById("left").style.backgroundColor = "green";
+		document.getElementById("right").style.backgroundColor = "green";
+		document.getElementById("up").style.backgroundColor = "green";
+		document.getElementById("coordx").innerHTML = y;
+	} else {
+		document.getElementById("down").style.backgroundColor = "red";
+		document.getElementById("up").innerHTML ="OH NO A WELL";
+	} 
+}
 function moveRight() {
-	x++;
-	document.getElementById("coordx").innerHTML = x;
-	}
+	if (map.substr(y * 6 + x++,1) == "a") {
+		x++;
+		document.getElementById("down").style.backgroundColor = "green";
+		document.getElementById("left").style.backgroundColor = "green";
+		document.getElementById("right").style.backgroundColor = "green";
+		document.getElementById("up").style.backgroundColor = "green";
+		document.getElementById("coordx").innerHTML = x;
+	} else {
+		document.getElementById("right").style.backgroundColor = "red";
+		document.getElementById("up").innerHTML ="OH NO A WEGALL";
+	} 
+}
 function moveLeft() {
-	x--;
-	document.getElementById("coordx").innerHTML = x;
-	}
+	if (map.substr(y * 6 + x--,1) == "a") {
+		x--;
+		document.getElementById("down").style.backgroundColor = "green";
+		document.getElementById("left").style.backgroundColor = "green";
+		document.getElementById("right").style.backgroundColor = "green";
+		document.getElementById("up").style.backgroundColor = "green";
+		document.getElementById("coordx").innerHTML = x;
+	} else {
+		document.getElementById("left").style.backgroundColor = "red";
+		document.getElementById("up").innerHTML ="OH NO A WZHEUFGH";
+	} 
+}
