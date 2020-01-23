78ac7104f09f2c9f5f3e7ae4d9266a8129488c05
OscarHavril <OscarHavril@users.noreply.github.com>
2020-01-23 18:43:23 +0100

    powoot

--- theCode.js
+++ theCode.js
@@ -1,75 +1,72 @@
-var map = "eeeeeeeeaaaaaeeaaaeeeeaeaaaeeaeeeaeeaeeaaeeeeeeee"
-var x = 2;
-var y = 2;
-document.getElementById("load").innerHTML = "JavaScript working!";
-document.getElementById("coordx").innerHTML = x;
-document.getElementById("coordy").innerHTML = y;
+var distLeft = 10000;
+//celsius here
+var biome = {
+	trees = {freq = 0.1, type = "oak", size = 0.95},
+	temp = 25,
+};
+var biomeWeight = {
+	trees : {freq : 0.1, type : "oak", size : 0.95},
+	temp = 20 //idk, seems low tho
+};
+var player = {
+	name = "",
+	sleep = 100,
+	alimentation = BodyNeeds(1, 1, 1, 1),
+	pyche = Psychologic(75, 50, 65)
+};
+function Psychologic(happiness, sociability, confidence) {
+	this.happiness = happiness;
+	this.sociability = sociability;
+	this.confidence = confidence;
+}
+function BodyNeeds(water, proteine, calcium, cereals) {
+	this.water = water;
+	this.proteine = proteine;
+	this.cereals = cereals;
+	this.calcium = calcium;
+}
+var input = document.getElementById("entry");
+input.addEventListener("keyup", function(event) {
+if (event.keyCode === 13) {
+event.preventDefault();
+document.getElementById("hollowButton").click();
+}
+});
+function execComm() {
+	var c = -1;
+	var command = document.getElementById("diary").value.toLowerCase();
+	console.log(command)
+	var CommandsList = ["help","exit","fire","chat","gather"]
+	for (let i = 0; i < CommandsList.length; i++) {
+		if(command==CommandsList[i] || command==CommandsList[i].substr(1,1)) {
+			c=i;
+		}
+	}
+	switch (c) {
+		case 0:
+			say("Commands: 'help' Show a list of commands. 'exit' Sleep trough the night. 'fire' Put on a campfire. 'chat' Talk with someone. 'gather' Gather food and water.");
+			break;
+		case 1:
+			say("You go to bed, and sleep until the sun rises.");
+			break;
+	
+		default:
+			say("Command not found. Type 'help' for help.");
+			break;
+	}
+}
+function say(text) {
+	document.getElementById("diary").innerHTML += "</br>"+text;
+}
+function GenerateRoad() {
+	//need to know how to change biomes
 
-function moveUp() {
-	if (map.substr((y-- * 7)-1 + x,1) == "a") {
-		y--;
-		document.getElementById("down").style.backgroundColor = "green";
-		document.getElementById("left").style.backgroundColor = "green";
-		document.getElementById("right").style.backgroundColor = "green";
-		document.getElementById("up").style.backgroundColor = "green";
-		document.getElementById("coordy").innerHTML = y;
-		document.getElementById("up").innerHTML ="MOVE YOUR BODY UPPPPPP GODAMN YOU!";
-		document.getElementById("down").innerHTML ="MOVE YOUR BODY NOTUP (y'know like down)";
-		document.getElementById("right").innerHTML ="MOVE YOUR BODY YAEH TO THE RIGHT";
-		document.getElementById("left").innerHTML ="MOVE. JUST MOVE. Where? Left.";
-	} else {
-		document.getElementById("up").style.backgroundColor = "red";
-		document.getElementById("up").innerHTML ="OH NO A WALL";
-	} 
-}
-function moveDown() {
-	if (map.substr((y++ * 7)-1 + x,1) == "a") {
-		y++;
-		document.getElementById("down").style.backgroundColor = "green";
-		document.getElementById("left").style.backgroundColor = "green";
-		document.getElementById("right").style.backgroundColor = "green";
-		document.getElementById("up").style.backgroundColor = "green";
-		document.getElementById("coordy").innerHTML = y;
-		document.getElementById("up").innerHTML ="MOVE YOUR BODY UPPPPPP GODAMN YOU!";
-		document.getElementById("down").innerHTML ="MOVE YOUR BODY NOTUP (y'know like down)";
-		document.getElementById("right").innerHTML ="MOVE YOUR BODY YAEH TO THE RIGHT";
-		document.getElementById("left").innerHTML ="MOVE. JUST MOVE. Where? Left.";
-	} else {
-		document.getElementById("down").style.backgroundColor = "red";
-		document.getElementById("down").innerHTML ="OH NO A WELL";
-	} 
-}
-function moveRight() {
-	if (map.substr((y * 7)-1 + x++,1) == "a") {
-		x++;
-		document.getElementById("down").style.backgroundColor = "green";
-		document.getElementById("left").style.backgroundColor = "green";
-		document.getElementById("right").style.backgroundColor = "green";
-		document.getElementById("up").style.backgroundColor = "green";
-		document.getElementById("coordx").innerHTML = x;
-		document.getElementById("up").innerHTML ="MOVE YOUR BODY UPPPPPP GODAMN YOU!";
-		document.getElementById("down").innerHTML ="MOVE YOUR BODY NOTUP (y'know like down)";
-		document.getElementById("right").innerHTML ="MOVE YOUR BODY YAEH TO THE RIGHT";
-		document.getElementById("left").innerHTML ="MOVE. JUST MOVE. Where? Left.";
-	} else {
-		document.getElementById("right").style.backgroundColor = "red";
-		document.getElementById("right").innerHTML ="OH NO A WEGALL";
-	} 
-}
-function moveLeft() {
-	if (map.substr((y-- * 7)-1 + x--,1) == "a") {
-		x--;
-		document.getElementById("down").style.backgroundColor = "green";
-		document.getElementById("left").style.backgroundColor = "green";
-		document.getElementById("right").style.backgroundColor = "green";
-		document.getElementById("up").style.backgroundColor = "green";
-		document.getElementById("coordx").innerHTML = x;
-		document.getElementById("up").innerHTML ="MOVE YOUR BODY UPPPPPP GODAMN YOU!";
-		document.getElementById("down").innerHTML ="MOVE YOUR BODY NOTUP (y'know like down)";
-		document.getElementById("right").innerHTML ="MOVE YOUR BODY YAEH TO THE RIGHT";
-		document.getElementById("left").innerHTML ="MOVE. JUST MOVE. Where? Left.";
-	} else {
-		document.getElementById("left").style.backgroundColor = "red";
-		document.getElementById("left").innerHTML ="OH NO A WZHEUFGH";
-	} 
+	//probably generate the end biome and then make the transitions.
+	//OR (more realistic) reeeeeeaaaaly slowly change the parameters each mile or so. Sounds clever. gonna do this.
+	/*HOW TO CHANGE PARAMETERS: 
+			1-generate a new biome (randomized)
+			2-Make an average of new-old whith more weight for the old 
+			(weight is predetremined, for example, whind things may change very fast so you should have a low weight. but a high weight on temperature bc "it's thermodinamics")
+			3-That's all i guess. Maybe a failsafe depending on zones, like wind may be weak but a weak wind will be waaaay stronger in mountains.*/
+	//WE NEED RULES !!!! THAT IS IMPERATIVE !!! (referencing to failsafe, yeah we need that)
 }
